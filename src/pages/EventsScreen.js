3d95db9ff7c692f4b95d856303343b6aa455302e
srikanth <srikanth916@gmail.com>
2023-05-12 09:23:13 -0400

    evening 2nd commit

--- src/pages/EventsScreen.js
+++ src/pages/EventsScreen.js
@@ -18,24 +18,23 @@ export default function EventsScreen() {
     const handleCloseDialog = () => {
         setOpenDialog(false);
     };
-
+   
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-4 col-md-4 col-sm-4 align-self-center d-flex" id="eventMap" style={{ backgroundImage: `url(${event1})`, borderRadius: '20px', position: 'relative' }}>
-                    <div className="location-wrapper">
-                        <img src={locationImage1} alt="Location" className="location-image1" onClick={() => handleLocationClick("Content for Location 1")} />
+                    <div>
+                        <img src={locationImage1} alt="Location" className="location-image1" id="imge1" onMouseOver={() => { document.getElementById('info').style.display = 'block'; }} onMouseOut={() => { document.getElementById('info').style.display = 'none'; }} />
+                        <p id="info" style={{ display: 'none' }}>Text to popup</p>
                     </div>
-                    <div className="location-wrapper">
-                        <img src={locationImage2} alt="Location" className="location-image2" onClick={() => handleLocationClick("Content for Location 2")} />
+                    <div>
+                        <img src={locationImage2} alt="Location" className="location-image2" />
                     </div>
-                    <div className="location-wrapper">
-                        <img src={locationImage3} alt="Location" className="location-image3" onClick={() => handleLocationClick("Content for Location 3")} />
+                    <div>
+                        <img src={locationImage3} alt="Location" className="location-image3" />
                     </div>
                 </div>
             </div>
-
-          
         </div>
     );
 }
